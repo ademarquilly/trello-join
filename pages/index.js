744b242d20116fb906cf699a6c73a44ae3d0b9e7
moicben <benjamain.georges@gmail.com>
2025-01-26 14:05:41 +0400

    header fixs

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    router.push('https://talentsgroupe.fr/');
+    router.push('https://phenixconcilium.fr/');
   }, [router]);
 
   return null;
