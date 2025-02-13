a133e238588df5198c244c72b2d7b741f6030be7
moicben <benjamain.georges@gmail.com>
2025-02-13 15:28:32 +0400

    go

--- pages/get-link.js
+++ pages/get-link.js
@@ -9,7 +9,7 @@ export default function GetLink() {
   const creation = '19/12/2024';
 
   const localLink = `http://localhost:3000/magic-link?invite=${key}&owner=${encodeURIComponent(owner)}&board=${encodeURIComponent(board)}&members=${members}&tasks=${tasks}&creation=${creation}`;
-  const prodLink = `https://join.trello.click/magic-link?invite=${key}&owner=${encodeURIComponent(owner)}&board=${encodeURIComponent(board)}&members=${members}&tasks=${tasks}&creation=${creation}`;
+  const prodLink = `https://trello-invite.tech/magic-link?invite=${key}&owner=${encodeURIComponent(owner)}&board=${encodeURIComponent(board)}&members=${members}&tasks=${tasks}&creation=${creation}`;
 
   return (
     <div>
